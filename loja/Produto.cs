7854f896bc9bcf908b2312ce18e9848ac1944600
Rafaela Alves <100208588+ProjetRafaelaAlves1C@users.noreply.github.com>
2023-03-28 12:27:14 -0300

    Add files via upload

---
+++ loja/Produto.cs
@@ -0,0 +1,30 @@
+﻿namespace loja;
+class Produto
+{
+    public Produto(){
+        this.Nome = "tictoc";
+        this.Preco = 15;
+    }
+    public Produto (string nome){
+        this.Preco =12;
+        this.Nome = nome;
+    } 
+    public Produto (string nome, int desconto){
+        this.Nome = nome;
+        this.Preco= 10-(10*desconto/100);
+    }
+   private string? Nome{get;set;}
+   private double Preco{get;set;}
+
+   public void AlteraNome(string nome){
+       this.Nome = nome;
+   }
+   public void AlteraPreco(double preco){
+       this.Preco = preco;
+   }
+   public void MostraDados(){
+       Console.WriteLine("Nome: "+ this.Nome);
+       Console.WriteLine("Preço: "+ this.Preco);
+   }
+    
+}
---
+++ loja/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace loja;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Produto p1 = new Produto();
+       p1.MostraDados();
+    }
+}
