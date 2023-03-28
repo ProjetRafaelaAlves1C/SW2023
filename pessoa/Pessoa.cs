3543dcaa3177b6f36301038acdac5d3643ac2350
Rafaela Alves <100208588+ProjetRafaelaAlves1C@users.noreply.github.com>
2023-03-28 12:18:47 -0300

    Add files via upload

---
+++ pessoa/Pessoa.cs
@@ -0,0 +1,33 @@
+﻿namespace pessoa;
+class Pessoa
+{
+    public Pessoa(){
+        this.Nome = "Nada";
+        this.Sobrenome = "Nada";
+        this.Idade = 0;
+    }
+    public Pessoa(string nome, int idade){
+        this.Nome = nome;
+        this.Sobrenome = "Nada";
+        this.Idade = idade;
+    }
+
+    private string? Nome {get;set;}
+    private string? Sobrenome{get;set;}
+    private int Idade{get;set;}
+    
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+    public void AlteraSobreNome(string sobrenome){
+        this.Sobrenome = sobrenome;
+    }
+    public void AlteraIdade(int idade){
+        this.Idade = idade;
+    }
+    public void Mostratudo(){
+        Console.WriteLine("Nome: "+ this.Nome);
+        Console.WriteLine("Sobrenome: " + this.Sobrenome);
+        Console.WriteLine("Idade: "+ this.Idade);
+    }
+}
---
+++ pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Rafa",18);
+        // p1.AlteraNome("Raffs");
+        // p1.AlteraSobreNome("Rosa");
+        // p1.AlteraIdade(16);
+        p1.Mostratudo();        
+    
+    }
+}
