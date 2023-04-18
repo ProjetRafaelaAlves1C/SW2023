8cd9d726d30351741bf914d60367bb018a844d8d
Rafaela Alves <100208588+ProjetRafaelaAlves1C@users.noreply.github.com>
2023-04-18 12:42:07 -0300

    Add files via upload

---
+++ atividadeJuros/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace atividadeJuros;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("");
+        Banco banco = new Banco();
+
+        banco.cliente ="Anderson";
+        Console.Write(banco.cliente + " digite o valor do seu emprestimo:");
+        banco.valor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Valor empréstimo: "+ banco.valor);
+        Console.WriteLine("Seus juros: "+ banco.juros);
+        Console.WriteLine("Juros mensais: "+ banco.mensal);
+
+        banco.saldo = 2000;
+        
+       /* 
+       double saldo = banco.consultaSaldo;
+        banco.limite = 300;
+        banco.deposito(30);
+        banco.deposito(30);
+        banco.limite = 300;
+        Console.WriteLine(banco.consultaSaldo());
+        double saldo = banco.consultaSaldo;
+        
+        /*
+        double saldo = banco.consultaSaldo;
+        banco.limite = 1500;
+        banco.saque();
+        banco.deposito();*/
+
+    }
+}
